8f0a35e35956dd39a7b428606f257463832bfc01
Jonathan Duffin <jonathan_duffin@hotmail.com>
2018-11-14 12:29:06 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(Array, String) {
+  var moreChocolateBars = [String, ...chocolateBars];
+  return moreChocolateBars;
+}
+function destructivelyAddElementToBeginningOfArray(Array, String) {
+  chocolateBars.unshift(String);
+  return chocolateBars;
+}
+function addElementToEndOfArray(Array, String) {
+  var moreChocolateBars = [String, ...chocolateBars];
+  return moreChocolateBars;
+}
+function destructivelyAddElementToEndOfArray(Array, String) {
+  chocolateBars.unshift(String);
+  return chocolateBars;
+}
